d231549f76be22e84dce0965495bfc43f843fba7
kh007im <mattkhojim@gmail.com>
2014-01-05 01:44:15 +0700

    Revert "Squashed update of kernel from 3.4.48 to 3.4.49"
    This reverts commit 99057e62802366efd259e3957ff14696a0f14f48.

--- drivers/gpu/drm/radeon/si.c
+++ drivers/gpu/drm/radeon/si.c
@@ -3876,12 +3876,6 @@ static int si_startup(struct radeon_device *rdev)
 	}
 
 	/* Enable IRQ */
-	if (!rdev->irq.installed) {
-		r = radeon_irq_kms_init(rdev);
-		if (r)
-			return r;
-	}
-
 	r = si_irq_init(rdev);
 	if (r) {
 		DRM_ERROR("radeon: IH init failed (%d).\n", r);
@@ -4047,6 +4041,10 @@ int si_init(struct radeon_device *rdev)
 		return r;
 	/* Memory manager */
 	r = radeon_bo_init(rdev);
+	if (r)
+		return r;
+
+	r = radeon_irq_kms_init(rdev);
 	if (r)
 		return r;
 
