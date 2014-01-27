2881580e16a56c9bd2a9b471b839950fb3617d7d
kh007im <mattkhojim@gmail.com>
2014-01-27 18:55:20 +0700

    asem kecut

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -61,8 +61,8 @@ static int msm_thermal_get_freq_table(void)
 	while (table[i].frequency != max_frequency)
 		i++;
 
-	limit_idx_low = 6;
-	limit_idx_high = limit_idx = i - 1;
+	limit_idx_low = 5;
+	limit_idx_high = limit_idx = i;
 	BUG_ON(limit_idx_high <= 0 || limit_idx_high <= limit_idx_low);
 fail:
 	return ret;
