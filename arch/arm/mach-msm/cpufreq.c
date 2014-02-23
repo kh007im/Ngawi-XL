56fcc37311effc2a0bb8cfe3d570672fb695a01d
kh007im <mattkhojim@gmail.com>
2014-02-23 16:22:03 +0700

    cpufreq:inject cpu turbo boost

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -60,6 +60,10 @@ struct cpu_freq {
 
 static DEFINE_PER_CPU(struct cpu_freq, cpu_freq_info);
 
+#ifdef CONFIG_TURBO_BOOST
+extern int msm_turbo(int);
+#endif
+
 /** cmdline defs **/
 uint32_t maxscroff = 0;
 uint32_t maxscroff_freq = 702000;
@@ -105,6 +109,10 @@ static int set_cpu_freq(struct cpufreq_policy *policy, unsigned int new_freq)
 			pr_debug("min: limiting freq to %d\n", new_freq);
 		}
 	}
+
+#ifdef CONFIG_TURBO_BOOST
+	new_freq = msm_turbo(new_freq);
+#endif
 
 	freqs.old = policy->cur;
 	freqs.new = new_freq;
